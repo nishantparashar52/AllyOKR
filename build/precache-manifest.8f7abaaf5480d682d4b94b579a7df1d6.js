a7d80da2e5d9a9d0f6807e77839035fbb27cd612
nishantparashar52 <nishantparashar52@users.noreply.github.com>
2021-09-04 22:59:43 +0530

    Add files via upload

---
+++ build/precache-manifest.8f7abaaf5480d682d4b94b579a7df1d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a277cb7efffa9f18c3d96961a016b3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "066082eb0bc92f70ef79",
+    "url": "/static/css/main.659b61f9.chunk.css"
+  },
+  {
+    "revision": "ac38bea7f7283ec7cee3",
+    "url": "/static/js/2.9637bfb9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9637bfb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "066082eb0bc92f70ef79",
+    "url": "/static/js/main.dfe6a427.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  }
+]);
